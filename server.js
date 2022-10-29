611a852ffcadb31ceb7ebb3dc8135bc0ad849272
Alikhaef2013 <38520870+Alikhaef2013@users.noreply.github.com>
2022-10-29 13:40:30 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const http = require("http");
 
 http.createServer(function(request, response) {    
    response.writeHead(200, {"Content-Type": "text/html"});
-   response.write("<h1>Hello, Node.js!</h1>");    
+   response.write("<h1>Hello, Node.js!</h1>");
+   response.write("<h2>Ali Khaef ~ f15j638</h2> \
+         <p>Running node server on port 3010</p>"); 
    response.end();
 }).listen(3010);
